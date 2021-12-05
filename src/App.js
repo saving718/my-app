420ba12c3bfa03818d351b2a0a3e8bf2813469ac
saving718 <ivanoffffff01@gmail.com>
2021-12-05 16:07:38 +0300

    comm

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Review from './review/review'
+
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Отзывы</h1>
+      <Review/>
+      <Review/>
+      <Review/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/review/review.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+import '../style/review.scss';
+
+export default function review() {
+    return (
+        <div className="b-review">
+            <div className="review__image">
+                <img src="people.png" alt="" />
+            </div>
+            <div className="review__title">Иван Иванов <br /> 05.12.2021</div>
+            <div className="review__description">Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.</div>
+        </div>
+    )
+}
